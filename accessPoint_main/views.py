a47e4921525771f9d3af0a721f497afa1676dafa
Justine-ini <ini_justine@outlook.com>
2025-06-12 01:11:02 +0100

    Added docstring to home function

--- accessPoint_main/views.py
+++ accessPoint_main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
+    """
+    Renders the home page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered 'home.html' template.
+    """
     return render(request, 'home.html')
