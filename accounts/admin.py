c89a983b1e380d17261fad940344bb73f7482d7c
Justine-ini <ini_justine@outlook.com>
2025-06-13 18:43:24 +0100

    Updated list display in user profile admin dashboard

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,9 +7,20 @@ from .models import User, UserProfile
 class CustomUserAdmin(UserAdmin):
     filter_horizontal = ()
     ordering = ('-date_joined',)
-    list_display = ('first_name', 'last_name', 'email', 'role', 'is_active', 'is_admin', 'is_superuser')
+    list_display = ('first_name', 'last_name', 'username', 'email', 'role', 'is_active', 'is_admin', 'is_superuser')
     list_filter = ()
     fieldsets = ()
 
+class UserProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = ()
+    list_display = ('get_username', 'country', 'state', 'city', 'created_at')
+    list_filter = ()
+    fieldsets = ()
+
+    def get_username(self, obj):
+        return obj.user.username if obj.user else None
+    
+    get_username.short_description = "Username"
+
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
