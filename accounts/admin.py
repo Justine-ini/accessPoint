99db365f5208915b62c671f6ea99d435ccf52108
Justine-ini <ini_justine@outlook.com>
2025-06-13 13:09:53 +0100

    feat(accounts): add accounts app with custom user model
    - Implement UserManager for user creation and authentication
    - Define a custom User model extending AbstractBaseUser
    - Add fields for user roles, authentication status, and timestamps
    - Set email as the unique identifier for authentication

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    ordering = ('-date_joined',)
+    list_display = ('first_name', 'last_name', 'email', 'role', 'is_active', 'is_admin', 'is_superuser')
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
---
+++ accounts/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email:str, password:str=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+    
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password) # Hash the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    RESTAURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer')
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+
+    # requiered fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    # Authentication fields
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = UserManager() # This tells User class what manager to use
+
+    def __str__(self):
+        return str(self.email) if self.email is not None else ""
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
