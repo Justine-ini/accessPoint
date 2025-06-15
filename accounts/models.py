36cf423511af489a8bcea72cace4ef499e24b52a
Justine-ini <ini_justine@outlook.com>
2025-06-15 18:24:13 +0100

    feat: improve account model string representation using full name

--- accounts/models.py
+++ accounts/models.py
@@ -3,16 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
 class UserManager(BaseUserManager):
-    def create_user(self, email:str, password:str=None, **extra_fields):
+    def create_user(self, email: str, password: str = None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
-    
+
         user = self.model(
             email=self.normalize_email(email),
             **extra_fields
         )
-        user.set_password(password) # Hash the password
+        user.set_password(password)  # Hash the password
         user.save(using=self._db)
         return user
 
@@ -24,7 +26,7 @@ class UserManager(BaseUserManager):
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
-        
+
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
 
@@ -32,11 +34,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    RESTAURANT = 1
+    VENDOR = 1
     CUSTOMER = 2
 
     ROLE_CHOICE = (
-        (RESTAURANT, 'Restaurant'),
+        (VENDOR, 'Vendor'),
         (CUSTOMER, 'Customer')
     )
 
@@ -45,7 +47,8 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=12, blank=True)
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICE, blank=True, null=True)
 
     # requiered fields
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -61,7 +64,7 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
-    objects = UserManager() # This tells User class what manager to use
+    objects = UserManager()  # This tells User class what manager to use
 
     def __str__(self):
         return str(self.email) if self.email is not None else ""
@@ -77,9 +80,12 @@ class User(AbstractBaseUser):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True, related_name='userprofile')
-    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
-    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, blank=True, null=True, related_name='userprofile')
+    profile_picture = models.ImageField(
+        upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(
+        upload_to='users/cover_photos', blank=True, null=True)
     address_line_1 = models.CharField(max_length=50, blank=True, null=True)
     address_line_2 = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=20, blank=True, null=True)
@@ -92,5 +98,4 @@ class UserProfile(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.email
-
+        return str(self.user.get_full_name())
