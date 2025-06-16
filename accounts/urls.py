b427730ca168dbfcf55a710bf12f66a010d222c3
Justine-ini <ini_justine@outlook.com>
2025-06-16 20:22:44 +0100

    Add URL routes for login, logout, myAccount, and role-based dashboards

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
     path('registerUser/', views.register_user, name='registerUser'),
     path('registerVendor/', views.register_vendor, name='registerVendor'),
+
+    path('my-account/', views.my_account, name='myAccount'),
+
+    path('customer-dashboard/', views.customer_dashboard,
+         name='customer-dashboard'),
+    path('vendor-dashboard/', views.vendor_dashboard, name='vendor-dashboard'),
 ]
