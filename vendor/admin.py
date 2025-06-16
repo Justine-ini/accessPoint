aeeedaa9bf37b11a2ab53453f93f0258516f3092
Justine-ini <ini_justine@outlook.com>
2025-06-16 20:28:53 +0100

    Rename VendorProfileAdmin to VendorAdmin for clarity

--- vendor/admin.py
+++ vendor/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Vendor
 
 
-class VendorProfileAdmin(admin.ModelAdmin):
+class VendorAdmin(admin.ModelAdmin):
     filter_horizontal = ()
     list_display = ('get_vendor', 'vendor_license',
                     'is_approved', 'created_at')
+    search_fields = ('vendor_name',)
     list_filter = ()
     fieldsets = ()
 
@@ -15,4 +16,4 @@ class VendorProfileAdmin(admin.ModelAdmin):
     get_vendor.short_description = "Vendor Name"
 
 
-admin.site.register(Vendor, VendorProfileAdmin)
+admin.site.register(Vendor, VendorAdmin)
