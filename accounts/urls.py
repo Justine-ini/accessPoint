f30044ef6f91ed623836510bd68f4e4c23c739a9
Justine-ini <ini_justine@outlook.com>
2025-06-18 22:28:13 +0100

    feat: add user activation route to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('customer-dashboard/', views.customer_dashboard,
          name='customer-dashboard'),
     path('vendor-dashboard/', views.vendor_dashboard, name='vendor-dashboard'),
+
+    path('activate/<uidb64>/<token>/',
+         views.activate, name='activate'),
 ]
