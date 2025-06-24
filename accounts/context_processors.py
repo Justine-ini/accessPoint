63d3cb05432b8feb0d842f5fc99b4d58c533e199
Justine-ini <ini_justine@outlook.com>
2025-06-24 12:35:07 +0100

    feat(context): provide vendor object globally via context processor

---
+++ accounts/context_processors.py
@@ -0,0 +1,13 @@
+from vendor.models import Vendor
+
+
+def get_vendor(request):
+    if request.user.is_authenticated:
+        try:
+            vendor = Vendor.objects.get(user=request.user)
+        except Vendor.DoesNotExist:
+            vendor = None
+    else:
+        vendor = None
+
+    return dict(vendor=vendor)
