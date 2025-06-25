f996c88a5ea2cfb50ef81f890e81ca06b0e31ace
Justine-ini <ini_justine@outlook.com>
2025-06-25 03:37:28 +0100

    feat: add image extension validation to VendorForm

--- vendor/forms.py
+++ vendor/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from .models import Vendor
+from accounts.validators import allow_only_images_validator
 
 
 class VendorForm(forms.ModelForm):
     """Form for creating and updating Vendor instances."""
+    vendor_license = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator]
+    )
+
     class Meta:
         """Meta options for VendorForm."""
         model = Vendor
