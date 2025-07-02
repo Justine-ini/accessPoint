9c8c6c33c181ceeea039d77cf59ff4bdab65e6d9
Justine-ini <ini_justine@outlook.com>
2025-07-02 18:42:01 +0100

    Update FoodItem model: add unique title, auto-capitalize title, and auto-generate slug

--- menu/models.py
+++ menu/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from vendor.models import Vendor
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -24,7 +25,7 @@ class Category(models.Model):
 class FoodItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    food_title = models.CharField(max_length=100)
+    food_title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=250, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -35,3 +36,11 @@ class FoodItem(models.Model):
 
     def __str__(self):
         return str(self.food_title)
+
+    def save(self, *args, **kwargs):
+        # Capitalize the name before saving
+        self.food_title = self.food_title.capitalize()
+        # Optionally auto-generate slug only if blank
+        if not self.slug:
+            self.slug = slugify(self.food_title)
+        super().save(*args, **kwargs)
