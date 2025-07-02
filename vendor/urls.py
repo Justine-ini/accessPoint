5bc9c52510bf78d0106389730ffc3fbbdddeca2f
Justine-ini <ini_justine@outlook.com>
2025-07-02 17:08:58 +0100

    Add URL patterns for editing and deleting FoodItems

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,3 +1,4 @@
+from django.utils.text import slugify
 from django.urls import path
 from accounts import views as AccountViews
 from .import views
@@ -9,13 +10,18 @@ urlpatterns = [
     path('menu-builder/category/<int:pk>/',
          views.fooditems_by_category, name='fooditems_by_category'),
 
-    # Category Crud
+    # Category CRUD
     path('menu-builder/category/add/', views.add_category, name='add_category'),
     path('menu-builder/category/edit/<int:pk>/',
          views.edit_category, name='edit_category'),
     path('menu-builder/category/delete/<int:pk>/',
          views.delete_category, name='delete_category'),
 
+    # Food Item CRUD
     path('menu-builder/fooditem/add/',
          views.add_fooditem, name='add_fooditem'),
+    path('menu-builder/fooditem/edit/<int:pk>/',
+         views.edit_fooditem, name='edit_fooditem'),
+    path('menu-builder/fooditem/delete/<int:pk>/',
+         views.delete_fooditem, name='delete_fooditem'),
 ]
