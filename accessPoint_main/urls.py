bb6654a967317c09b13238966c8a47dcba0bc855
Justine-ini <ini_justine@outlook.com>
2025-07-05 13:21:00 +0100

    add marketplace URLs to project routes

--- accessPoint_main/urls.py
+++ accessPoint_main/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('', include('accounts.urls')),
+    path('marketplace/', include('marketplace.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
