233b9ed540e9e228dfc3ef595cdc5673fba8f6fb
Justine-ini <ini_justine@outlook.com>
2025-07-10 03:38:35 +0100

    update(FoodItem): added related_name to category foreign key

--- menu/models.py
+++ menu/models.py
@@ -24,7 +24,8 @@ class Category(models.Model):
 # Fooditem or Product
 class FoodItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='fooditems')
     food_title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=250, blank=True)
