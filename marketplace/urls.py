559a20e4cd4acef15b44ac32c49110677ac01a50
Justine-ini <ini_justine@outlook.com>
2025-07-10 03:51:44 +0100

    update marketplace URLs with dynamic vendor and cart paths
    - Includes vendor detail, add_to_cart, and decrease_cart routes

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path("", views.marketplace, name="marketplace"),
+    path("<slug:slug>/", views.vendor_detail, name="vendor_detail"),
+
+    # Add to cart
+    path("add_to_cart/<int:food_id>/", views.add_to_cart, name="add_to_cart"),
+    path('decrease-cart/<int:food_id>/',
+         views.decrease_cart, name='decrease_cart'),
 ]
