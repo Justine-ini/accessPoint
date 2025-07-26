c59b2f0dd09abf3bc05dcfd8b904ca32fbd390af
Justine-ini <ini_justine@outlook.com>
2025-07-26 09:54:00 +0100

    Add get_cart_amounts context processor
    Compute cart subtotal, tax, and total for authenticated users in marketplace/context_processors.py

--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -1,5 +1,6 @@
 from django.db import DatabaseError
 from .models import Cart
+from marketplace.models import FoodItem
 
 
 def get_cart_counter(request):
@@ -25,3 +26,21 @@ def get_cart_counter(request):
         except DatabaseError:
             cart_count = 0
     return {'cart_count': cart_count}
+
+
+def get_cart_amounts(request):
+    tax = 0
+    total = 0
+    subtotal = 0
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+        if cart_items.exists():
+            for item in cart_items:
+                fooditem = FoodItem.objects.get(pk=item.fooditem.id)
+                subtotal += fooditem.price * item.quantity
+            total = subtotal + tax
+    return {
+        'cart_subtotal': subtotal,
+        'cart_tax': tax,
+        'cart_total': total
+    }
