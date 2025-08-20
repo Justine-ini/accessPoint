8e511397e004ffa2093912c2987b44bceb47400d
Justine-ini <ini_justine@outlook.com>
2025-08-20 18:40:06 +0100

    Add checkout URL route

--- accessPoint_main/urls.py
+++ accessPoint_main/urls.py
@@ -30,6 +30,9 @@ urlpatterns = [
     # Cart
     path('cart/', marketplaceViews.cart, name='cart'),
     path('search/', marketplaceViews.search, name='search'),
+
+    # Checkout
+    path('checkout/', marketplaceViews.checkout, name='checkout'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
