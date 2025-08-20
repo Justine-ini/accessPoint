331e4e570d2352ef8c8d5c0bedc6e606d66f061b
Justine-ini <ini_justine@outlook.com>
2025-08-20 18:36:59 +0100

    Fix get_user_profile bug for unauthenticated users
    - Added explicit else case to set user_profile=None
    
    - Prevents UnboundLocalError when request.user is not authenticated

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -21,6 +21,8 @@ def get_user_profile(request):
             user_profile = UserProfile.objects.get(user=request.user)
         except UserProfile.DoesNotExist:
             user_profile = None
+    else:
+        user_profile = None
 
     return dict(user_profile=user_profile)
 
