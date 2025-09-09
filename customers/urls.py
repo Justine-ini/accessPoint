c835471ac97ab44b2c59f5811def8b47cc94a28a
Justine-ini <ini_justine@outlook.com>
2025-09-09 13:53:18 +0100

    feat(customers): add URL patterns for my_orders and order_detail

--- customers/urls.py
+++ customers/urls.py
@@ -5,4 +5,8 @@ from .import views
 urlpatterns = [
     path('', AccountViews.customer_dashboard, name='customer_dashboard'),
     path('profile/', views.customer_profile, name='customer_profile'),
+    path('my-orders/', views.my_orders, name='customer_my_orders'),
+    path('order-detail/<str:order_number>/', views.order_detail,
+         name='order_detail'),
+
 ]
