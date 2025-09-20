5f6df088d5d9cec799678daa4ddd07ee5d7a3c20
Justine-ini <ini_justine@outlook.com>
2025-09-20 17:22:53 +0100

    Add get_total_order_by_vendor helper
    - Parse total_data JSON for vendor
    
    - Return subtotal, VAT, and total

--- orders/utils.py
+++ orders/utils.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+import json
 import random
+from datetime import datetime
 
 
 def generate_order_number(pk, prefix="ORD", region="NG"):
     """
-    Generates a unique order number string using a 
+    Generates a unique order number string using a
         prefix, region, current timestamp, primary key, and a random number.
 
     Args:
@@ -13,9 +14,22 @@ def generate_order_number(pk, prefix="ORD", region="NG"):
         region (str, optional): The region code to include in the order number. Defaults to "NG".
 
     Returns:
-        str: A unique order number in the format 
+        str: A unique order number in the format
             '{PREFIX}-{REGION}-{YYYYMMDDHHMMSS}{pk}-{RANDOM6DIGITS}'.
     """
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     random_part = str(random.randint(100000, 999999))
     return f"{prefix.upper()}-{region.upper()}-{current_time}{pk}-{random_part}"
+
+
+def get_total_order_by_vendor(order, vendor_id):
+
+    total_data = json.loads(order.total_data)
+    data = total_data.get(str(vendor_id))
+    # Total, Tax and Subtotal
+    context = {
+        "subtotal": data["subtotal"],
+        "tax": data["tax_data"]["VAT"]["7.00"],
+        "total": data["total"],
+    }
+    return context
